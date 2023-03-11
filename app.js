6c1eb4b3108240b043d218b81e3c25fcb462bc7f
Lakshay Dureja <lakshaydureja7@gmail.com>
2023-03-12 02:37:56 +0530

    google o auth

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const encrypt = require("mongoose-encryption");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var findOrCreate = require('mongoose-findorcreate')
 
 
 
@@ -30,17 +32,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 
 userSchema.plugin(passportLocalMongoose);
-
-
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
@@ -49,11 +52,29 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
 
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function (req, res) {
     res.render("home");
 });
+
+
+app.get("/auth/google",function(req,res){
+    passport.authenticate("google",{scope:["profile"]})
+})
+
 app.get("/login", function (req, res) {
     res.render("login");
 });
